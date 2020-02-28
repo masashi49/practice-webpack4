9d6f248969584ddfd4b08217d6cb770162ec61d2
山田将史 <masashisama94@gmail.com>
2020-02-28 15:30:41 +0900

    webpack4を使う

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {  // メインとなるJavaScriptファイル（エントリーポイント）
+
+    mode: "development",
+    entry: `./src/index.js`,
+    // ファイルの出力設定  
+    output: {    //  出力ファイルのディレクトリ名
+        path: `${ __dirname }/dist`,    // 出力ファイル名  
+        filename: "main.js"
+    },
+    devServer: {
+        contentBase: "dist",
+        open: true
+    }
+};
